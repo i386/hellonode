d85b091793dce9f190e4918047e4a9548f3dc574
James William Dumay <jdumay@cloudbees.com>
2018-01-10 12:58:39 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello James and Mic");
+  response.end("Hello Node");
 }
 var www = http.createServer(handleRequest);
 www.listen((process.env.PORT || 8080));
